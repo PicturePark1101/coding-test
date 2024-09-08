e6a94c9ee96cc9c45a3a3e968ca473ca29fc5ded
PicturePark1101 <seojin5565@gmail.com>
2024-09-08 18:55:43 +0900

    [sliver5] 2751 - 수 정렬하기 2 #8

---
+++ SJ/src/part02/ch07/Main2751.java
@@ -0,0 +1,25 @@
+package part02.ch07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main2751 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int[] nums = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      nums[i] = Integer.parseInt(br.readLine());
+    }
+
+    Arrays.sort(nums);
+
+    for (int i = 0; i < N; i++) {
+      System.out.println(nums[i]);
+    }
+    br.close();
+  }
+}
